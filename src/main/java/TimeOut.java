3c56e4fe0c0cee94d6cf5a39ca05507c668867e9
selina.yang <yun0836@naver.com>
2026-05-24 16:01:24 +0900

    feat: 타이머 제한 시간 1분으로 수정

--- src/main/java/TimeOut.java
+++ src/main/java/TimeOut.java
@@ -2,10 +2,12 @@ public class TimeOut extends Thread {
     @Override
     public void run() {
         try {
-            Thread.sleep(30000);
+            Thread.sleep(1000 * 60);    // 제한 시간 1분
+
             System.out.println();
             System.out.println("----------------------");
             System.out.println("이용 시간이 모두 지나 서비스를 종료합니다.");
+
             System.exit(0);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
